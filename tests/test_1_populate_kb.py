951cbfaa159dec630d1ca11127bd48b6c009d1ff
Jesper Friis <jesper-friis@users.noreply.github.com>
2024-06-11 10:48:38 +0200

    Update test_1_populate_kb.py

--- tests/test_1_populate_kb.py
+++ tests/test_1_populate_kb.py
@@ -13,3 +13,8 @@ def test_populate_kb():
     ts = Triplestore(backend="rdflib")
     populate_triplestore(ts, indir / "resources.yaml")
     ts.serialize(outdir / "kb.ttl")
+
+    # Note, we do not check directly against the KB. Instead we use our
+    # APIs for such tests. This is done in `test_2_generate_pipeline` and 
+    `test_3_generate_workchain`. 
+    
